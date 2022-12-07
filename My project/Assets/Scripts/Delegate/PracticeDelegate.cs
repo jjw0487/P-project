edaac39f6cc32f3d2a5445f57090b1af5e549e11
Chase Jeon <jjw0487@gmail.com>
2022-12-08 05:27:07 +0900

    1208 first Commit

---
+++ My project/Assets/Scripts/Delegate/PracticeDelegate.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events; // 유니티 이벤트와 액션 기능을 사용 
+
+
+
+public class PracticeDelegate : MonoBehaviour
+{
+
+    public UnityEvent onInputSpace;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            onInputSpace.Invoke();
+        }
+    }
+}
