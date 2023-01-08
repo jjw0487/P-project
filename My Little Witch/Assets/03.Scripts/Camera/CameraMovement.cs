decfbe732332908521bd1d0d6a27717ca24f8cbe
jjw0487 <jjw0487@gmail.com>
2023-01-09 00:06:08 +0900

    0109

--- My Little Witch/Assets/03.Scripts/Camera/CameraMovement.cs
+++ My Little Witch/Assets/03.Scripts/Camera/CameraMovement.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using static UnityEngine.Rendering.DebugUI.Table;
 
 public class CameraMovement : MonoBehaviour
 {
@@ -6,6 +7,7 @@ public class CameraMovement : MonoBehaviour
     [SerializeField] private float lerpspeed;
     [SerializeField] private Vector2 ZoomRange = new Vector2(2.0f, 11.0f);
     [SerializeField] private float ZoomSpeed;
+    [SerializeField] private float rotateSpeed = 10.0f;
 
     Vector3 myDir = Vector3.zero;
     float targetDist = 0.0f;
@@ -27,6 +29,14 @@ public class CameraMovement : MonoBehaviour
         dist = Mathf.Lerp(dist, targetDist, Time.deltaTime * 5.0f);
 
         this.transform.position = Vector3.Lerp(this.transform.position, myTarget.transform.position + myDir * dist, lerpspeed);
+
+/*
+        Vector3 rot = myTarget.transform.rotation.eulerAngles; // 현재 카메라의 각도를 Vector3로 반환
+        rot.y += Input.GetAxis("Mouse X") * rotateSpeed; // 마우스 X 위치 * 회전 스피드
+                                                         //rot.x += -1 * Input.GetAxis("Mouse Y") * rotateSpeed; // 마우스 Y 위치 * 회전 스피드
+        Quaternion q = Quaternion.Euler(rot); // Quaternion으로 변환
+        this.transform.rotation = Quaternion.Slerp(transform.rotation, q, 0.5f); // 자연스럽게 회전
+*/
     }
 
 }
