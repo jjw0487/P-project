30c58cd898dc16b5ceb64167ac4e6f5c9ec9b197
jjw0487 <jjw0487@gmail.com>
2023-01-31 21:49:50 +0900

    0131  in class

--- My Little Witch/Assets/03.Scripts/UI/Inventory/Item.cs
+++ My Little Witch/Assets/03.Scripts/UI/Inventory/Item.cs
@@ -13,6 +13,8 @@ public class Item : MonoBehaviour
 
     public ItemInformation myItem;
     public GameObject[] contents;
+
+    private bool moveOn = false;
     private void Awake()
     {
         if (contents == null) { contents = null; }
@@ -54,19 +56,38 @@ public class Item : MonoBehaviour
         //아이템 위치를 오브젝트 풀에 담을 수 있도록 연구 해 보자.
         for (int i = 0; i < SceneData.Inst.Inven.slots.Length; ++i) // 슬롯 수량만큼 반복
         {
-            if (SceneData.Inst.Inven.slots[i].childCount == 1) // 자식이 하나만 있다면
+            if (SceneData.Inst.Inven.slots[i].GetComponent<Slots>().item == null) // slot에 item 정보가 null이라면
             {
-                this.transform.SetParent(SceneData.Inst.Inven.slots[i]); // 부모로 빈 슬롯
-                this.transform.position = new Vector3(999f, 999f, 999f); // 게임 화면에서 보이지 않도록 임의의 포지션에 위치시켜볼까?
-                SceneData.Inst.Inven.slots[i].GetComponent<Slots>().AddItem(this.GetComponent<Item>(), myItem.curNumber);
-                // slot 상에 이미지를 변경
+                for(int n = 0; n < SceneData.Inst.Inven.slots.Length; ++n) // item 위치를 임의로 옮겼을지도 모르니 한 번 더 검사
+                {
+                    if(SceneData.Inst.Inven.slots[n].GetComponent<Slots>().item != null) // item 정보가 null이 아니라면
+                    {
+                        if (SceneData.Inst.Inven.slots[n].GetComponent<Slots>().item.myItem.orgData.name == myItem.orgData.name)
+                            //이름이 같은지 조건검사
+                        {
+                            moveOn = true; 
+                            this.transform.position = new Vector3(999f, 999f, 999f);
+                            //SceneData.Inst.Inven.slots[n].GetComponentInChildren<Item>().myItem.curNumber += myItem.curNumber;
+                            SceneData.Inst.Inven.slots[n].GetComponent<Slots>().AddCount(this.myItem.curNumber);
+                            break;
+                        }
+                    }
+                }
+                if(!moveOn) // 위에 조건에 안걸렸을 때 실행
+                {
+                    this.transform.SetParent(SceneData.Inst.Inven.slots[i]); // 부모로 빈 슬롯
+                    this.transform.position = new Vector3(999f, 999f, 999f); // 게임 화면에서 보이지 않도록 임의의 포지션에 위치시켜볼까?
+                    SceneData.Inst.Inven.slots[i].GetComponent<Slots>().AddItem(this.GetComponent<Item>(), myItem.curNumber);
+                    // slot 상에 이미지를 변경
+                }
+
+                moveOn = false;
                 break; // 조건검사에 걸린다면 반복문 탈출
             }
-            else if (SceneData.Inst.Inven.slots[i].GetComponentInChildren<Item>().myItem.orgData.name == myItem.orgData.name)
+            else if (SceneData.Inst.Inven.slots[i].GetComponent<Slots>().item.myItem.orgData.name == myItem.orgData.name)
             {
                 //this.transform.SetParent(SceneData.Inst.Inven.slots[i]);
                 this.transform.position = new Vector3(999f, 999f, 999f);
-                SceneData.Inst.Inven.slots[i].GetComponentInChildren<Item>().myItem.curNumber += myItem.curNumber; // 수량을 증가
                 SceneData.Inst.Inven.slots[i].GetComponent<Slots>().AddCount(this.myItem.curNumber); // 증가 한 후에 다시 화면에 표현
                 break;
             }
@@ -76,8 +97,6 @@ public class Item : MonoBehaviour
 
     public void GetItemFromInteractableItems(GameObject what)
     {
-
-
         //아이템 위치를 오브젝트 풀에 담을 수 있도록 연구 해 보자.
         for (int i = 0; i < SceneData.Inst.Inven.slots.Length; ++i) // 슬롯 수량만큼 반복
         {
